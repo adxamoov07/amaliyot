15909fef9a9dc9cb505409c242dcb31c42df4403
adxamoov07 <adxamovabdulloh06@gmail.com>
2023-06-21 16:57:26 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,147 @@
+// const person = {
+//     username: "username",
+//     firstname: "firstname",
+//     age:  00,
+//     country: "uzb",
+//     nation: "uzbek",
+//     isMarried: false,
+//     hasCar: null,
+
+// }
+
+// console.log(person.username)
+// const Iam = {
+//     username: "adhamov",
+//     firstname: "abdulloh",
+//     age: 16,
+//     country: "uzb",
+//     nation: "uzbek",
+//     isMarried: false,
+//     hasCar: null,
+
+// }
+
+// console.log(Iam.username)
+
+
+// const brather = {
+//     username: "ibrohim",
+//     firstname: "adxamov",
+//     age: 20,
+//     country: "uzb",
+//     nation: "uzbek",
+//     isMarried: false,
+//     hasCar: null,
+
+// }
+
+// console.log(brather.age)
+
+
+// const father = {
+//     username: "ilhomboy",
+//     firstname: "yunusov",
+//     age: 41,
+//     country: "uzb",
+//     nation: "uzbek",
+
+// }
+
+// console.log(father.age)
+
+
+// let frend = {
+//     username: "muhammad",
+//     firstname: "oripov",
+//     age: 17,
+//     country: "uzb",
+//     nation: "uzbek",
+//     isMarried: false,
+//     hasCar: null,
+
+// }
+
+// console.log(frend.username)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let student = {
+    username: "username",
+    firstname: "firstname",
+    age:  00,
+    country: "uzb",
+    nation: "uzbek",
+    isMarried: false,
+    hasCar: null,
+
+}
+
+console.log(student.username)
+
+
+let teacher = {
+    username: "BAHROMJON",
+    firstname: "ABDULHAYEV",
+    age: not,
+    country: "uzb",
+    nation: "uzbek",
+    
+}
+
+console.log(Iam.username)
+
+
+let english = {
+    username: "ibrohim",
+    firstname: "adxamov",
+    age: 20,
+    country: "uzb",
+    nation: "uzbek",
+    isMarried: false,
+    hasCar: null,
+
+}
+
+console.log(english.age)
+
+
+let penFrend = {
+    username: "Ziyoviddin",
+    firstname: "obidov",
+    age: 16,
+    country: "uzb",
+    nation: "uzbek",
+
+}
+
+console.log(penFrend.age)
+
+
+const comyuter = {
+    username: "nomsiz",
+    firstname: "nomsiz",
+    age: not,
+    country: "china",
+    nation: "pekin",
+
+}
+
+console.log(comyuter.username)
+
